6a9244d9590fabd3c0fc49ccb08a94ff51e445ac
Mohamed Mbarkiou <mbarkioumed@outlook.com>
2024-12-21 19:16:47 +0100

    sss

--- backend/crud-application/src/main/java/com/crud/crud/application/repository/NoteRepository.java
+++ backend/crud-application/src/main/java/com/crud/crud/application/repository/NoteRepository.java
@@ -1,6 +1,5 @@
 package com.crud.crud.application.repository;
 
-import com.crud.crud.application.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.crud.crud.application.model.Note;
